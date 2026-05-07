c633a86d0a307144053ded12b3f1eda47034f923
priceptor2112-ai <priceptor2112@gmail.com>
2026-05-07 04:50:59 +0300

    feat: add common films endpoint

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,66 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+
+import java.util.Collection;
+
+@Slf4j
+@RestController
+@RequestMapping("/films")
+public class FilmController {
+    private final FilmService filmService;
+
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film body) {
+        return filmService.addNewFilm(body);
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film body) {
+        return filmService.updateFilm(body.getId(), body);
+    }
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return filmService.getAllFilms();
+    }
+
+    @GetMapping("/{id}")
+    public Film findById(@PathVariable Long id) {
+        return filmService.getFilmById(id)
+                .orElseThrow(() -> new NotFoundException("Film with id " + id + " not found"));
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLike(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.removeLike(id, userId);
+    }
+
+    @GetMapping("/popular")
+    public Collection<Film> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
+        return filmService.getFilmsByLikes(count);
+    }
+
+    @GetMapping("/common")
+    public Collection<Film> getCommonFilms(@RequestParam Long userId,
+                                           @RequestParam Long friendId) {
+        return filmService.getCommonFilms(userId, friendId);
+    }
+}
